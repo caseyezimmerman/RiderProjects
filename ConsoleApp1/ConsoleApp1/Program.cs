41be69c47c523959b34dcd18f1e283af9b367a28
Casey Zimmerman <caseyezimmerman@gmail.com>
2019-01-04 10:03:43 -0500

    tutorials

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Dog
+    {
+        public string Type;
+        public int Year;
+        public string Color;
+
+        public Dog(string type, int year, string color)
+        {
+            Type = type;
+            Year = year;
+            Color = color;
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Dog bailey = new Dog("border collie", 5, "black and white");
+            Dog cooper = new Dog("boxer", 1, "brown");
+            Dog lila = new Dog("pom", 3, "light brown");
+            Console.WriteLine(bailey.Type);
+            Console.WriteLine(cooper.Year);
+            Console.WriteLine(lila.Color);
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 2, 3, 4, 5};
+            int rotations = 5;
+            for (int i = 0; i < rotations; i++)
+            {
+                Console.WriteLine(nums[0]);
+            }
+        }
+    }
+}
